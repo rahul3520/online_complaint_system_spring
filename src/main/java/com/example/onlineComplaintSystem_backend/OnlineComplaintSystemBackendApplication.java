f7fe43bfd9d9c8671c1998d359c334843e9dee9b
rahulg3520 <rahul@nestgroup.net>
2022-12-09 15:00:34 +0530

    backend

---
+++ src/main/java/com/example/onlineComplaintSystem_backend/OnlineComplaintSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineComplaintSystem_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineComplaintSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineComplaintSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onlineComplaintSystem_backend/OnlineComplaintSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineComplaintSystem_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineComplaintSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
